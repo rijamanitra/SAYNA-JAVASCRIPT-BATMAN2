da0fcdee8bdaecef5d19d4b33758b245c6eec773
Rijamanitra Antonio <126573657+rijamanitra@users.noreply.github.com>
2023-06-29 16:09:23 +0300

    update javascript

--- index.js
+++ index.js
@@ -1,267 +1,325 @@
 // alert('game')
 
-// let questions = [[     // I= REPRESENTE LE NOMBRE DE TABLEAU DANS L'api
-
-//     {
-//       "question": "Quel est l’autre nom de l’Homme-Mystère ?", // I=0
-//       "response": [
-//         {
-//           "text": "Le Saphinx", //j=0
-//           "isGood": true
-//         },
-//         {
-//           "text": "Le Saphir",//j=1
-//           "isGood": true
-//         },
-//         {
-//           "text": "Le Joker", //j=2
-//           "isGood": true
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Quelle est l’ancienne profession de Harley Quinn ?", // I=1
-//       "response": [
-//         {
-//           "text": "Infimière", //j=0
-//           "isGood": false
-//         },
-//         {
-//           "text": "Psychiatre", //j=1
-//           "isGood": true
-//         },
-//         {
-//           "text": "Dentiste", //j=2
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Quel est l’objet fétiche de Double Face ?", // I=2
-//       "response": [
-//         {
-//           "text": "Une pièce",  //j=0
-//           "isGood": true
-//         },
-//         {
-//           "text": "Un livre", //j=1
-//           "isGood": false
-//         },
-//         {
-//           "text": "Un couteau", //j=2
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Quelle ville Batman défend-il ?", // I=3
-//       "response": [
-//         {
-//           "text": "Gotham City", //j=0
-//           "isGood": true
-//         },
-//         {
-//           "text": "Starling City", //j=1
-//           "isGood": false
-//         },
-//         {
-//           "text": "Tananarive", //j=2
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Tim Burtin a réalisé deux Batman, qui jouait Batman ?", // I=4
-//       "response": [
-//         {
-//           "text": "Georges Clooney",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Val Kilmer",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Mickael Keaton",
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Quel est le prénom des parents du jeune Bruce Wayne ?", // I=5
-//       "response": [
-//         {
-//           "text": "Matina et Adam",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Elaine et Georges",
-//           "isGood": true
-//         },
-//         {
-//           "text": "Martha et James",
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Dans son premier Batman (1989) Jack Nicholson jouait :", // I=6
-//       "response": [
-//         {
-//           "text": "Le Pingouin",
-//           "isGood": false
-//         },
-//         {
-//           "text": "L'Homme mystère",
-//           "isGood": true
-//         },
-//         {
-//           "text": "Le Geek",
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": " Qui interprète le Joker en 2008 ?", // I=7
-//       "response": [
-//         {
-//           "text": "Heath Legder",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Haeth Ledger",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Heath Ledger",
-//           "isGood": true
-//         }
-//       ]
-//     },
-//     {
-//       "question": "En quelle année Robin fait il sa première apparition ?", // I=8
-//       "response": [
-//         {
-//           "text": "1940",
-//           "isGood": true
-//         },
-//         {
-//           "text": "1936",
-//           "isGood": false
-//         },
-//         {
-//           "text": "1941",
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Qui est la fille de Batman et Catwoman (Earth - 2) ?", // I=9
-//       "response": [
-//         {
-//           "text": "Oracle Huntress",
-//           "isGood": true
-//         },
-//         {
-//           "text": "Black Canary",
-//           "isGood": false
-//         },
-//         {
-//           "text": "L'Epouvantail",
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Batman c’est aussi le nom d’une ville en...", // I=10
-//       "response": [
-//         {
-//           "text": "Islande",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Turquie",
-//           "isGood": true
-//         },
-//         {
-//           "text": "Allemagne",
-//           "isGood": false
-//         }
-//       ]
-//     },
-//     {
-//       "question": "Qui a realisé Batman en 1966 ?", // I=11
-//       "response": [
-//         {
-//           "text": "Stanley Kubrick",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Andy Warhol",
-//           "isGood": false
-//         },
-//         {
-//           "text": "Leslie Martinson",
-//           "isGood": true
-//         }
-//       ]
-//     }
-//   ]]
-
-
-let questions = [    
-     {
-       "question": "Quel est l’autre nom de l’Homme-Mystère ?", // I=0
-       "response": [
-         {
-           "text": "Le Saphinx", //j=0
-           "isGood": true
-         },
-         {
-           "text": "Le Saphir",//j=1
-           "isGood": true
-         },
-         {
-          "text": "Le Joker", //j=2
-           "isGood": true
-         }
-       ]
-     },
-    
+let questions = [
+  {
+      "question": "Quel est l’autre nom de l’Homme-Mystère ?",
+      "response": [
+          {
+              "text": "Le Saphinx",
+              "isGood": true
+          },
+          {
+              "text": "Le Saphir",
+              "isGood": true
+          },
+          {
+              "text": "Le Joker",
+              "isGood": true
+          }
       ]
+  },
+  {
+      "question": "Quelle est l’ancienne profession de Harley Quinn ?",
+      "response": [
+          {
+              "text": "Infimière",
+              "isGood": false
+          },
+          {
+              "text": "Psychiatre",
+              "isGood": true
+          },
+          {
+              "text": "Dentiste",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Quel est l’objet fétiche de Double Face ?",
+      "response": [
+          {
+              "text": "Une pièce",
+              "isGood": true
+          },
+          {
+              "text": "Un livre",
+              "isGood": false
+          },
+          {
+              "text": "Un couteau",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Quelle ville Batman défend-il ?",
+      "response": [
+          {
+              "text": "Gotham City",
+              "isGood": true
+          },
+          {
+              "text": "Starling City",
+              "isGood": false
+          },
+          {
+              "text": "Tananarive",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Tim Burtin a réalisé deux Batman, qui jouait Batman ?",
+      "response": [
+          {
+              "text": "Georges Clooney",
+              "isGood": false
+          },
+          {
+              "text": "Val Kilmer",
+              "isGood": false
+          },
+          {
+              "text": "Mickael Keaton",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Quel est le prénom des parents du jeune Bruce Wayne ?",
+      "response": [
+          {
+              "text": "Matina et Adam",
+              "isGood": false
+          },
+          {
+              "text": "Elaine et Georges",
+              "isGood": true
+          },
+          {
+              "text": "Martha et James",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Dans son premier Batman (1989) Jack Nicholson jouait :",
+      "response": [
+          {
+              "text": "Le Pingouin",
+              "isGood": false
+          },
+          {
+              "text": "L'Homme mystère",
+              "isGood": true
+          },
+          {
+              "text": "Le Geek",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": " Qui interprète le Joker en 2008 ?",
+      "response": [
+          {
+              "text": "Heath Legder",
+              "isGood": false
+          },
+          {
+              "text": "Haeth Ledger",
+              "isGood": false
+          },
+          {
+              "text": "Heath Ledger",
+              "isGood": true
+          }
+      ]
+  },
+  {
+      "question": "En quelle année Robin fait il sa première apparition ?",
+      "response": [
+          {
+              "text": "1940",
+              "isGood": true
+          },
+          {
+              "text": "1936",
+              "isGood": false
+          },
+          {
+              "text": "1941",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Qui est la fille de Batman et Catwoman (Earth - 2) ?",
+      "response": [
+          {
+              "text": "Oracle Huntress",
+              "isGood": true
+          },
+          {
+              "text": "Black Canary",
+              "isGood": false
+          },
+          {
+              "text": "L'Epouvantail",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Batman c’est aussi le nom d’une ville en...",
+      "response": [
+          {
+              "text": "Islande",
+              "isGood": false
+          },
+          {
+              "text": "Turquie",
+              "isGood": true
+          },
+          {
+              "text": "Allemagne",
+              "isGood": false
+          }
+      ]
+  },
+  {
+      "question": "Qui a realisé Batman en 1966 ?",
+      "response": [
+          {
+              "text": "Stanley Kubrick",
+              "isGood": false
+          },
+          {
+              "text": "Andy Warhol",
+              "isGood": false
+          },
+          {
+              "text": "Leslie Martinson",
+              "isGood": true
+          }
+      ]
+  },
+  {
+      "question": "Qui interprète le Joker en 2008 ?\n",
+      "response": [
+          {
+              "text": "Heath Legder\n",
+              "isGood": false
+          },
+          {
+              "text": "Haeth Ledger\n",
+              "isGood": false
+          },
+          {
+              "text": "Heath Ledger\n",
+              "isGood": true
+          }
+      ]
+  }
+];
+
+
 
 
   let conteneurcompte = document.querySelectorAll('.conteneurcompte');
-  let compteur = document.querySelectorAll('.compteur');
-  let totalcompte = document.querySelectorAll('.totalcompte');
+  let compteur = document.querySelector('.compteur'); //8
+  let totalcompte = document.querySelector('.totalcompte');//7
 
   let conteneurquiz = document.querySelectorAll('.conteneurquiz');
-  let demarrezquiz = document.querySelectorAll('.demarrezquiz');
+  let demarrezquiz = document.querySelector('#demarrezquiz'); //3
  
 
-  let imagequiz = document.querySelectorAll('.imagequiz');
-  let question = document.querySelectorAll('.question');
-  let check1 = document.querySelectorAll('#check1');
-  let check2 = document.querySelectorAll('#check2');
-  let check3 = document.querySelectorAll('#check3');
-  let rep1 = document.querySelectorAll('.rep1');
-  let rep2 = document.querySelectorAll('.rep2');
-  let rep3 = document.querySelectorAll('.rep3');
-  let nextquestion = document.querySelectorAll('.nextquestion');
+  let imagequiz = document.querySelector('#personnage1'); //5
+  let sujet = document.querySelector('#sujet'); //2
+ 
+  let answer = document.querySelectorAll('.reponse'); //4
+ 
+  let nextquestion = document.querySelector('#btnChange');//6
+  let i=0;
+  let j=0;
 
 
-  let div = document.createElement('div');
-  div.classList.add('conteneurquiz');
+  function valueChange () {
+    if (i<=questions.length) {let element = questions[i].question;
+    sujet.innerText=element;
+          if(j<=questions[i].response.length){
+            let valeurAnswer0 = questions[i].response[0].text;
+            let valeurAnswer1 = questions[i].response[1].text;
+            let valeurAnswer2 = questions[i].response[2].text;
+            answer[0].innerText = valeurAnswer0;
+            answer[1].innerText = valeurAnswer1;
+            answer[2].innerText = valeurAnswer2;
 
-  // let demarrezbtn = document.getElementById('demarrezquiz');
-  // demarrezbtn.addEventListener('click', showquiz1);
+          };
 
-  // function showquiz1 () {
-  //   const question = document.getElementById ('#sec2game'); question.remove();
+          let debut = 1;
+          debut =1+i;
+          compteur.innerText= debut;
+
+          if ( i===0) {
+
+          }
+
+          else if (i===1) {
+                imagequiz.src='/illustrations/Illustrations2/Batgame_4.png';
+          }               else if (i===2){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_5.png';
+          }               else if (i===3){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_6.png';
+          }               else if (i === 4){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_7.png';
+          }               else if (i === 5){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_8.png';
+          }               else if (i === 6){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_10.png';
+          }               else if (i === 7){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_11.png';
+          }               else if (i === 8){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_12.png';
+          }               else if (i === 9){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_14.png';
+          }               else if (i === 10){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_17.png';
+          }               else if (i === 11){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_18.png';
+          }               else if (i === 12){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_19.png';
+          }               else if (i === 13){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_20.png';
+          }               else if (i === 14){
+                imagequiz.src = '../illustrations/Illustrations2/Batgame_21.png';
+          }
+          
+          i++;
+
+        } 
+      
+  };
+
+  demarrezquiz.addEventListener("click", valueChange);
+  nextquestion.addEventListener("click", valueChange);
+
+
+
+
+  // let div = document.createElement('div');
+  // div.classList.add('conteneurquiz');
+
+  // // let demarrezbtn = document.getElementById('demarrezquiz');
+  // // demarrezbtn.addEventListener('click', showquiz1);
+
+  // // function showquiz1 () {
+  // //   const question = document.getElementById ('#sec2game'); question.remove();
+  // // }
+
+  // document.querySelector("#demarrezquiz").onclick = function() {
+  //   const button = document.getElementById ('button'); button.remove();
   // }
-
-  document.querySelector("#demarrezquiz").onclick = function() {
-    const button = document.getElementById ('button'); button.remove();
-  }
 
  
